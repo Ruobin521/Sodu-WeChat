04caf3c0440a5364b4f27fbf9747af8dbfc23ef6
dang <ruobin521@gmail.com>
2018-03-28 15:46:58 +0800

    first commit

---
+++ components/bookitem/bookitem.js
@@ -0,0 +1,34 @@
+// components/bookitem/bookitem.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    title: {
+      type: String,
+      value: '这是书名',
+    },
+    time: {
+      type: String,
+      value: '这是书名',
+    },
+    subtitle: {
+      type: String,
+      value: '这是书名',
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
